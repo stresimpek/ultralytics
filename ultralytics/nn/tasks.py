5cfd549dc0b8b3ffb22a982dacf53b8adbb28d0d
stresimpek <jordan.josdaan@gmail.com>
2025-05-13 21:47:20 +0700

    Update tasks.py
    Signed-off-by: stresimpek <jordan.josdaan@gmail.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1429,8 +1429,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             C2fPSA,
             C2fCIB,
             C2PSA,
-            A2C2f,
-            CBAM
+            A2C2f
         }
     )
     for i, (f, n, m, args) in enumerate(d["backbone"] + d["head"]):  # from, number, module, args
@@ -1502,6 +1501,11 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             c2 = args[0]
             c1 = ch[f]
             args = [*args[1:]]
+        elif m is CBAM:
+            c1, c2 = ch[f], args[0]
+            if c2 != nc:
+                c2 = make_divisible(min(c2, max_channels) * width, 8)
+            args = [c1, *args[1:]]
         else:
             c2 = ch[f]
 
