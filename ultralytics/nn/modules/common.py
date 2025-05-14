44085005a67a0a21a5a17d2427f1a0a0319eeeed
stresimpek <jordan.josdaan@gmail.com>
2025-05-14 22:42:46 +0700

    Update common.py
    Signed-off-by: stresimpek <jordan.josdaan@gmail.com>

--- ultralytics/nn/modules/common.py
+++ ultralytics/nn/modules/common.py
@@ -1,1 +1,26 @@
+import torch
+import torch.nn as nn
 
+class SE(nn.Module):
+    """Squeeze-and-Excitation block - lightweight attention mechanism.
+    
+    Args:
+        c (int): Number of input/output channels
+        r (int): Reduction ratio for channels in the SE block (default=16)
+    """
+    def __init__(self, c, r=16):
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc1 = nn.Conv2d(c, c // r, kernel_size=1, bias=False)
+        self.act = nn.SiLU(inplace=True)  # SiLU activation for better performance
+        self.fc2 = nn.Conv2d(c // r, c, kernel_size=1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        module_input = x
+        x = self.avg_pool(x)
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        return module_input * x
