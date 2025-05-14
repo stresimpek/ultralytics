00e32cf5244a1f92cec72aebcc2d310c8092b761
stresimpek <jordan.josdaan@gmail.com>
2025-05-14 22:46:38 +0700

    Update tasks.py
    Signed-off-by: stresimpek <jordan.josdaan@gmail.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -68,7 +68,8 @@ from ultralytics.nn.modules import (
     YOLOEDetect,
     YOLOESegment,
     v10Detect,
-    CBAM
+    CBAM,
+    SE
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, YAML, colorstr, emojis
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
