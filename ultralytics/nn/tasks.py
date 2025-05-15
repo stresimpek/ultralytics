864256b71248ace37d26d8c4e21ae62990bdaeaf
stresimpek <jordan.josdaan@gmail.com>
2025-05-15 13:15:39 +0700

    Update tasks.py
    Signed-off-by: stresimpek <jordan.josdaan@gmail.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1412,9 +1412,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             SCDown,
             C2fCIB,
             A2C2f,
-            CBAM,
-            ASFF,
-            BiFPN
+            CBAM
         }
     )
     repeat_modules = frozenset(  # modules with 'repeat' arguments
